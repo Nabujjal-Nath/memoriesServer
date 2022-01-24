9ee632263489e0f94566c0341b1308a1757ab82c
nabujjal <nabujjal@gmail.com>
2022-01-25 00:49:51 +0530

    mongodb cloud atlas connection

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import express from express;
+import express from 'express';
 import bodyParser from "body-parser";
-import cors from cors;
-import Mongoose from "mongoose";
+import cors from 'cors';
+import mongoose from "mongoose";
 
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://memories:Zxcvbnm123!@cluster0.9kdtz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 4000;
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => { console.log(`Server is up at port:${PORT}`) }))
+    .catch((error) => console.log(error.message))
