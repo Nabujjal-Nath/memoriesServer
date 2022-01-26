046f24a09255c1d3c19cfddaa284e08ee025c33c
nabujjal <nabujjal@gmail.com>
2022-01-26 14:14:35 +0530

    create post added

--- controller/routesDefinition.js
+++ controller/routesDefinition.js
@@ -2,9 +2,20 @@ import PostMessage from "../models/postMessages.js"
 
 export const getPosts = async (req, res) => {
     try {
-        const postMessages = await PostMessage.find()
+        const postMessages = await PostMessage.find();
         res.status(200).json(postMessages)
     } catch (error) {
         res.status(404).json({ message: error.message })
+    }
+}
+
+export const createPosts = async (req, res) => {
+    const post = req.body;                     //getting data from client side ie. form data
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+        res.status(400).json({ newPost })
+    } catch (error) {
+        res.status(400).json({ message: error.message })
     }
 }
